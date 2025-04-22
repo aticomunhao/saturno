a8c1d2641a41d4819f9760d3dd0973be71581ea1
willianmmarinho <willianmmarinho@gmail.com>
2025-04-22 13:25:04 -0300

    Conserto de Models 4

--- database/seeders/CfopSeeder.php
+++ database/seeders/CfopSeeder.php
@@ -4,7 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use App\Models\Cfop;
+use App\Models\ModelCfop;
 
 class CfopSeeder extends Seeder
 {

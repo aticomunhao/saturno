f3e512fbccbb9b85e1e6c15eedd4aa656ef7fe8d
MathiasColetti <mathiasc2004@gmail.com>
2025-07-02 14:17:51 -0300

    Edit

--- resources/views/relacao-deposito-setor/index.blade.php
+++ resources/views/relacao-deposito-setor/index.blade.php
@@ -7,50 +7,74 @@
 @section('content')
     <br>
     <div class="container">
-        <div class="card">
-            <h5 class="card-header" style="color:blue">Gerenciar Relação Depósito/Setor</h5>
+        <div class="card shadow-sm">
+            <h5 class="card-header text-primary">Gerenciar Relação Depósito/Setor</h5>
             <div class="card-body">
-                <p class="card-text">
-                <div class="row justify-content-beteween">
-                    <div class="col-sm-12 col-md-3"></div>
-                    <div class="col-sm-12 col-md-3"></div>
-                    <div class="col-sm-12 col-md-3">
-                        <a href="{{ route('relacao-deposito-setor.create') }}"><button class="btn btn-success"
-                                style="width: 100%">Novo</button></a>
+                <form>
+                    <div class="row g-3 align-items-end mb-3">
+                        <div class="col-12 col-md-4">
+                            <label for="id_setor_id" class="form-label">Setor</label>
+                            <select class="form-select select2 w-100" name="setor_id" id="id_setor_id">
+                                @foreach ($setores as $setor)
+                                    <option value="{{ $setor->id }}">{{ $setor->nome }} - {{ $setor->sigla }}</option>
+                                @endforeach
+                            </select>
+                        </div>
+
+                        <div class="col-12 col-md-3">
+                            <label for="id_deposito_id" class="form-label">Depósito</label>
+                            <select class="form-select select2 w-100" name="deposito_id" id="id_deposito_id">
+                                @foreach ($depositos as $deposito)
+                                    <option value="{{ $deposito->id }}">{{ $deposito->nome }} - {{ $deposito->sigla }}
+                                    </option>
+                                @endforeach
+                            </select>
+                        </div>
+
+                        <div class="col-12 col-md-3 d-grid">
+                            <label class="form-label invisible">Novo</label>
+                            <a href="{{ route('relacao-deposito-setor.create') }}" class="btn btn-success w-100">
+                                Novo
+                            </a>
+                        </div>
                     </div>
-                </div>
+                </form>
+
                 <hr>
-                <table class="table table-sm table-striped table-bordered border-secondary table-hover align-middle">
-                    <thead>
-                        <tr style="background-color: #d6e3ff; font-size:14px; color:#000000">
-                            <th scope="col">Deposito</th>
-                            <th scope="col">Setor Responsavel</th>
-                            <th scope="col">Data Inicio Responsabilidade</th>
-                            <th scope="col">Ações</th>
 
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @foreach ($relacoes_deposito_setor as $relacao)
+                <div class="table-responsive">
+                    <table class="table table-sm table-striped table-bordered border-secondary table-hover align-middle">
+                        <thead class="text-dark text-center">
                             <tr>
-                                <td>{{ $relacao->Deposito->nome }}</td>
-                                <td>{{ $relacao->Setor->nome }}</td>
-                                <th scope="col">
-                                    {{ $relacao->dt_inicio ? Carbon::parse($relacao->dt_inicio)->format('d/m/Y') : '--' }}
-                                </th>
-
-                                <a href="{{ route('relacao-deposito-setor.show', $relacao->id) }}"
-                                    class="btn btn-outline-primary btn-sm" style="font-size: 1rem; color: #303030"
-                                    data-placement="top" title="Editar"><i class="bi bi-search"></i></a>
-                                <a href="{{ route('relacao-deposito-setor.edit', $relacao->id) }}"
-                                    class="btn btn-outline-warning btn-sm" style="font-size: 1rem; color: #303030"
-                                    data-placement="top" title="Editar"><i class="bi bi-pencil"></i></a>
-                                </td>
+                                <th scope="col">Depósito</th>
+                                <th scope="col">Setor Responsável</th>
+                                <th scope="col">Data Início Responsabilidade</th>
+                                <th scope="col">Ações</th>
                             </tr>
-                        @endforeach
-                    </tbody>
-                </table>
-                </p>
+                        </thead>
+                        <tbody>
+                            @foreach ($relacoes_deposito_setor as $relacao)
+                                <tr>
+                                    <td>{{ $relacao->Deposito->nome }}</td>
+                                    <td>{{ $relacao->Setor->nome }}</td>
+                                    <td>{{ $relacao->dt_inicio ? Carbon::parse($relacao->dt_inicio)->format('d/m/Y') : '--' }}
+                                    </td>
+                                    <td class="text-center">
+                                        <a href="{{ route('relacao-deposito-setor.show', $relacao->id) }}"
+                                            class="btn btn-outline-primary btn-sm me-1" title="Visualizar">
+                                            <i class="bi bi-search"></i>
+                                        </a>
+                                        <a href="{{ route('relacao-deposito-setor.edit', $relacao->id) }}"
+                                            class="btn btn-outline-warning btn-sm" title="Editar">
+                                            <i class="bi bi-pencil"></i>
+                                        </a>
+                                    </td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+
             </div>
         </div>
     </div>
